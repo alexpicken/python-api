696547b07ad6296012465c6c5890dd5953ba094c
Alexander Picken <pickeale@students.zhaw.ch>
2022-05-21 14:23:11 +0200

    21.05

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from webbrowser import Elinks
 from flask import Flask, request
 
 import spacyprocess
 import neo4jclass
+import random
 
 
 app = Flask(__name__)
@@ -20,23 +22,29 @@ def webhook():
     action = query_result.get("action")
     session_id = req.get("session").split("/")[-1]
     user_id = req.get("originalDetectIntentRequest").get("payload").get("userId")
-    test = query_result.get("outputContexts")
-    #if query_result["outputContexts"]:
-        #for context in query_result.get("outputContexts"):
-            #if(query_result.get)
-            #family_topic = query_result.get("outputContexts").get("")
-            #work_topic =
-            #hobby_topic =
-            #studies_topic = 
+    contexts = query_result.get("outputContexts")
 
     if (action == "chatbotgreetings.chatbotgreetings-custom" or action == "input.welcome"):
         neo4jclass.add_session_graphs(user_id, session_id)
-    #elif (action == "input.unknown"):
-        #topic_list = [family_topic, work_topic, hobby_topic, studies_topic]
+    
+    elif (action == "input.unknown"):
+        topic_list = []
+        for context in contexts:
+            if context["name"].split("/")[-1] == "session-work":
+                topic_list.append(context["parameters"]["work"])
+            elif context["name"].split("/")[-1] == "session-studies":
+                topic_list.append(context["parameters"]["studies"])
+            elif context["name"].split("/")[-1] == "session-family":
+                topic_list.append(context["parameters"]["family"])
+            elif context["name"].split("/")[-1] == "session-hobby":
+                topic_list.append(context["parameters"]["hobby"])
+        random_int = random.randint(0, len(topic_list))
+        if topic_list[random_int]:
+            query_response = "I'd like to talk more about your " + topic_list[random_int] +", what else can you tell me about that?"
+        
 
     spacyprocess.analyse(session_id, query_text)
 
     return {
-        "fulfillmentText": test
-        #"fulfillmentText": query_response
+        "fulfillmentText": query_response
     }
