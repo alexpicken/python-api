ba5936d12d1e8c69994d97a3a6089792035c6464
Alexander Picken <pickeale@students.zhaw.ch>
2022-05-21 14:46:50 +0200

    21.05

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ def webhook():
     user_id = req.get("originalDetectIntentRequest").get("payload").get("userId")
     contexts = query_result.get("outputContexts")
 
+    spacyprocess.analyse(session_id, query_text)
+
     if (action == "chatbotgreetings.chatbotgreetings-custom" or action == "input.welcome"):
         neo4jclass.add_session_graphs(user_id, session_id)
     
@@ -39,12 +41,8 @@ def webhook():
             elif context["name"].split("/")[-1] == "session-hobby":
                 topic_list.append(context["parameters"]["hobby"])
         random_int = random.randint(0, len(topic_list))
-        query_response = str(topic_list) + str(random_int)
-        #if topic_list[random_int]:
-            #query_response = "I'd like to talk more about your " + topic_list[random_int] +", what else can you tell me about that?"
-        
-
-    spacyprocess.analyse(session_id, query_text)
+        if random_int < len(topic_list):
+            query_response = "I'd like to talk more about your " + topic_list[random_int] +", what else can you tell me about that?"
 
     return {
         "fulfillmentText": query_response
